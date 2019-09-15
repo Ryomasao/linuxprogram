04ea901b2eb073df7f6f131887ca74061a22cca1
tohu.masao <konoemario@gmail.com>
2019-09-15 20:19:16 +0900

    catを別ディレクトリに分割

---
+++ README.md
@@ -0,0 +1,10 @@
+# Make 基本
+
+Makefile は以下の構文
+
+`target`が`prereq`より古い場合、command が実行される
+
+```
+target: prereq1
+  command
+```
---
+++ cat/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+try() {
+  fileName="$1"
+  make
+  ./cat $fileName
+}
+
+echo "引数で渡されたファイルを標準出力に出力すt"
+try test.txt
