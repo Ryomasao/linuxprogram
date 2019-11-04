06fbaef598b318ab759be2b0a831e80ee8016372
tohu.masao <konoemario@gmail.com>
2019-11-04 13:43:02 +0900

    TODO記載

--- src/90_web/web.c
+++ src/90_web/web.c
@@ -2,12 +2,23 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+static void service(FILE *in, FILE *out, char *docroot);
+
 int main(int argc, char *argv[])
 {
-  printf("Hello World \n");
+  // TODO 引数のチェック
+  // TODO signal補足処理の初期設定
+  service(stdin, stdout, "docroot");
   exit(0);
 }
 
+static void service(FILE *in, FILE *out, char *docroot)
+{
+  // TODO リクエストパース
+  // TODO レスポンス処理
+}
+
+// TODO エラーログ処理
 //static void log_exit(char *fmt, ...)
 //{
 //  va_list ap;
@@ -19,6 +30,8 @@ int main(int argc, char *argv[])
 //  exit(1);
 //}
 //
+
+// TODO メモリアロケート処理
 //static void *xmalloc(size_t sz)
 //{
 //  void *p;
