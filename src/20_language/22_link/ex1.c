e3a9933e8f7b61f7a6246b31706b0e47446010c2
Ryomasao <konoemario@gmail.com>
2019-12-30 19:53:09 +0900

    static付きのグローバル変数の挙動を確認

--- src/20_language/22_link/ex1.c
+++ src/20_language/22_link/ex1.c
@@ -2,17 +2,26 @@
 
 #include "ex1.h"
 
+
 int f(int);
 int f2(void);
+int static_value(int);
 
+// global scopeで定義している
 int x = NUMBER;
 int y = 2;
+int z = 999;
+// 暗黙のextern
+// 宣言だけしていて、f2.cで定義している
 int a;
+
+
 
 int main(void) {
   printf("x=%d\n", x);
   printf("f(x)=%d\n", f(x));
   printf("f2(void)=%d\n", f2());
   printf("main f2 a=%d\n", a);
+  printf("static_value(z)=%d\n", static_value(z));
   return 0;
 }
